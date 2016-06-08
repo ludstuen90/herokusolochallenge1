98089d72d5454256eed1a9692750eb13eb6ec698
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-06-08 11:40:19 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,9 @@
+
+var http = require('http');
+
+http.createServer(function(req, res){
+ // res.writeHead(200);
+ console.log("Made it to create server");
+ // res.write('Hello World!');
+ // res.end();
+}).listen(process.env.PORT || 3000);
